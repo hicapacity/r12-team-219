d719ee8c7c8a694896160ed384e148c2e44185e8
Julie Ann Sakuda <jsakuda@gmail.com>
2012-10-12 21:18:59 -1000

    Adding markdown related js files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require bootstrap.min
+//= require showdown
+//= require to-markdown
+//= require editor
 //= require jquery
 //= require jquery_ujs
 //= require_tree .

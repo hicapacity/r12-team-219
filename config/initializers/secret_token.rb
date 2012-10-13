c0b8bca1fd94872df8f7ec036eaa4c60288cb769
James Wang <jianshi.wang@gmail.com>
2012-10-12 17:03:02 -0700

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nexus::Application.config.secret_token = 'e8a6813ceb74461c138d60da0da3b096ae1637b7c84d962aa1fd21d9896aa1c5706592d640dce18073d33d622a1b742b969daabda77609e147c6aed5031c35a1'

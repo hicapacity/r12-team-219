ad787912edf35d2b5062d27e3f61201c0b86d29b
chemicwepn <chemicwepn@gmail.com>
2012-10-13 12:27:29 -1000

    Devise Setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013002215) do
+ActiveRecord::Schema.define(:version => 20121013065002) do
 
   create_table "pages", :force => true do |t|
     t.string   "url_path"
@@ -23,5 +23,26 @@ ActiveRecord::Schema.define(:version => 20121013002215) do
   end
 
   add_index "pages", ["url_path"], :name => "index_pages_on_url_path", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "nick_name"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end

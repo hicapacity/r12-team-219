18d0f2ffbd2efa8e182c4af25d27cbb67877d31f
James Wang <jianshi.wang@gmail.com>
2012-10-13 14:43:51 -1000

    Removing token route + home resource.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,4 @@ class HomeController < ApplicationController
 
   def index
   end
-
-  def token
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Nexus::Application.routes.draw do
   devise_for :users
-
-  resources :home, :only => :index
   get 'pages' => 'pages#index'
   get 'pages/*url_path' => 'pages#show'
   post 'pages/*url_path' => 'pages#create'
 
   root :to => 'home#index'
 
-  match '/token' => 'home#token', :as => :token
-  authenticated :user do
-    root :to => 'home#index'
-  end
+  # authenticated :user do
+  #  root :to => 'home#index'
+  # end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.

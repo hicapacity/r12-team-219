dbde7629e924d100c3f1b76c4a4771227b5c509a
James Wang <jianshi.wang@gmail.com>
2012-10-14 02:11:17 -1000

    Fixed migration for page.commit.

--- db/migrate/20121014005525_remove_commit_fields_from_page.rb
+++ db/migrate/20121014005525_remove_commit_fields_from_page.rb
@@ -1,6 +1,5 @@
 class RemoveCommitFieldsFromPage < ActiveRecord::Migration
   def change
-    remove_column :pages, :commit
     remove_column :pages, :author
     remove_column :pages, :markdown
   end

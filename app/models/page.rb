e3e6e358abb9b2a18855fbaeebff7628e875bf2d
Bob Matcuk <bmatcuk@gmail.com>
2012-10-13 17:23:12 -1000

    Put script back into page#show

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,20 @@ class Page < ActiveRecord::Base
   has_many :commits
   accepts_nested_attributes_for :commits
   attr_accessible :url_path, :timestamps, :commits_attributes
+  
+  def title
+    get_latest_version unless @latest_commit
+    @latest_commit.title
+  end
+  
+  def markdown
+    get_latest_version unless @latest_commit
+    @latest_commit.markdown
+  end
+  
+  protected
+
+  def get_latest_version
+    @latest_commit = self.commits.ordered_by_date.first
+  end
 end

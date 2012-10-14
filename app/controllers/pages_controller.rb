b6626405cb66638be06ad4d1da1824c0f8ca67af
James Wang <jianshi.wang@gmail.com>
2012-10-13 17:06:30 -1000

    Commits json seeds updated.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,3 @@
-# curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"page":{"title":"testpage","author":"nobody","markdown":"# Hallo — editing Markdown in WYSIWYG"}} ' http://slickage-x-hicap.r12.railsrumble.com/pages/hallojs
-# curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"page":{"title":"testpage","author":"nobody","markdown":"[Hallo](/hallo/) is the simplest web editor imaginable. Instead of cluttered forms or toolbars, you edit your web content as it is. Just you, your web design, and your content."}} ' http://slickage-x-hicap.r12.railsrumble.com/pages/hallojs/foo
-# curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"page":{"title":"testpage","author":"nobody","markdown":"**Note:** you can also edit the Markdown source below"}} ' http://slickage-x-hicap.r12.railsrumble.com/pages/hallojs/bar
-
 class PagesController < ApplicationController
   respond_to :html, :json
   def index
--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,5 +1,13 @@
 class Commit < ActiveRecord::Base
   belongs_to :user
   belongs_to :page
-  attr_accessible :oid, :title
+  attr_accessible :oid, :title, :markdown
+
+  def markdown
+    @markdown
+  end
+
+  def markdown=(val)
+    @markdown = val
+  end
 end
